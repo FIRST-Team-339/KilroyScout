81267609cd517f0e4bf50820cd66dbe5ccfcd17a
fisherjacobc <fisherjc2008@gmail.com>
2024-04-05 14:57:30 -0400

    fix: dark mode improvements

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   }) {
     return (
       <html lang="en">
-        <body className="min-h-screen flex transition-all">
+        <body className="min-h-screen flex transition">
           <Sidebar />
           <main className="h-screen w-full p-8 flex overflow-scroll dark:bg-gray-950 dark:text-gray-50">
             {children}
