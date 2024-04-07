4d62afefcc4b8bf4061a079d17735556b3b10dea
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2024-04-07 17:28:38 -0400

    chore(web): use nativewind

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,57 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { registerRootComponent } from "expo";
-import useBLE from './hooks/useBLE';
-import { useState } from 'react';
-import ConnectDevice from "./components/ConnectDevice";
+// import useBLE from './hooks/useBLE';
+// import { useState } from 'react';
+// import ConnectDevice from "./components/ConnectDevice";
 
+import "./styles.css";
 
 export default function App() {
-  const {
-    requestPermissions,
-    scanForPeripherals,
-    allDevices,
-    connectToDevice,
-    connectedDevice,
-    heartRate,
-    disconnectFromDevice,
-  } = useBLE();
+  // const {
+  //   requestPermissions,
+  //   scanForPeripherals,
+  //   allDevices,
+  //   connectToDevice,
+  //   connectedDevice,
+  //   heartRate,
+  //   disconnectFromDevice,
+  // } = useBLE();
 
-  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  // const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
-  const scanForDevices = async () => {
-    const isPermissionsEnabled = await requestPermissions();
-    if (isPermissionsEnabled) {
-      scanForPeripherals();
-    }
-  };
+  // const scanForDevices = async () => {
+  //   const isPermissionsEnabled = await requestPermissions();
+  //   if (isPermissionsEnabled) {
+  //     scanForPeripherals();
+  //   }
+  // };
 
-  const hideModal = () => {
-    setIsModalVisible(false);
-  };
+  // const hideModal = () => {
+  //   setIsModalVisible(false);
+  // };
 
-  const openModal = async () => {
-    scanForDevices();
-    setIsModalVisible(true);
-  };
+  // const openModal = async () => {
+  //   scanForDevices();
+  //   setIsModalVisible(true);
+  // };
   
   return (
-    <View style={{ flex: 1, justifyContent: "center",
-    alignItems: "center" }}>
-      <TouchableOpacity
-        onPress={connectedDevice ? disconnectFromDevice : openModal}
-      >
-        <Text>
-          {connectedDevice ? "Disconnect" : "Connect"}
-        </Text>
-      </TouchableOpacity>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <ConnectDevice
-        closeModal={hideModal}
-        visible={isModalVisible}
-        connectToPeripheral={connectToDevice}
-        devices={allDevices}
-      />
     </View>
   );
 }
