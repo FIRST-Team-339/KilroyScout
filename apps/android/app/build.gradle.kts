a74e1133f4969467bc69a468d743b7eb942ce905
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2025-02-19 23:13:02 -0500

    chore: build data

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 22
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "2025.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -20,7 +20,11 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            isDebuggable = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        debug {
+            isDebuggable = true
         }
     }
     compileOptions {
